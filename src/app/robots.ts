5e8c8ac954d8f48aa4884bfb31a8b83e2dfdcddd
agamenonmacondo <agamenonmacondo@gmail.com>
2025-12-22 01:34:30 -0500

    robot.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,11 +2,13 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/api/', '/admin/'],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/', '/admin/'],
+      },
+    ],
     sitemap: 'https://www.ccs724.com/sitemap.xml',
   };
 }
