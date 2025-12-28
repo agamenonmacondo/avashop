95890de1b4a4e23b9bba100e992a6565632a513e
agamenonmacondo <agamenonmacondo@gmail.com>
2025-12-27 21:47:45 -0500

    head

---
+++ src/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="icon" href="/favicon.ico?v=2" />
+      <link rel="icon" sizes="96x96" href="/favicon-96x96.png?v=2" />
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png?v=2" />
+      <meta name="theme-color" content="#ffffff" />
+    </>
+  );
+}
